b97d8372d80e5bc134b69de80357d9e069f07036
joeholden <91440840+joeholden@users.noreply.github.com>
2021-09-28 21:45:00 -0500

    Add files via upload

---
+++ working.py
@@ -0,0 +1,174 @@
+import PIL
+import matplotlib
+from PIL import Image
+from matplotlib.colors import LinearSegmentedColormap
+from matplotlib import colors, colorbar, cm
+import matplotlib.pyplot as plt
+import math
+import image_slicer
+import os
+import time
+
+# my_colors = ['#fde725', '#9fda3a', '#4ac26e', '#1fa187', '#27a187', '#365c8d', '#47327f', '#440154']
+# my_colors = ['#ee82ee', '#4b0082', '#0000ff', '#008000', '#ffff00', '#ffa500', '#ff0000']
+# my_colormap = LinearSegmentedColormap.from_list('my_colormap', my_colors, N=500)
+my_colormap = plt.get_cmap('inferno')
+
+IMAGE_PATH = 'binary.tif'
+PIL.Image.MAX_IMAGE_PIXELS = 9999999999
+
+large_image = Image.open(IMAGE_PATH)
+i_width, i_height = large_image.size
+# ~1000x1000 px tiles
+num_rows = math.ceil(i_height / 1000)
+num_cols = math.ceil(i_width / 1000)
+
+
+def split_grid(directory, full_image_path):
+    tiles = image_slicer.slice(full_image_path, num_rows * num_cols, save=False)
+    image_slicer.save_tiles(tiles, directory=directory, prefix='slice', format='png')
+    sample_tile_file = os.listdir('tiles')[0]
+    sample_tile = Image.open(f'tiles/{sample_tile_file}')
+    global tile_dims
+    tile_dims = (sample_tile.height, sample_tile.width)
+    print('Finished Splitting Grid')
+
+
+def get_binary_density_and_recolor(image_path, tile_position):
+    im = Image.open(image_path)
+    # mode 'L' is greyscale. We want RGB because we have a binary image + a colored ROI -> 8bit
+    if im.mode == 'L':
+        im = im.convert('RGB')
+    height, width = im.size
+
+    # pixel counts
+    count_1 = 0
+    count_0 = 0
+    count_red = 0
+
+    # loading a copy pixel map to manipulate and a reference to copy from
+    pixel_map = im.load()
+    img = Image.new(im.mode, im.size)
+    pixels_new = img.load()
+
+    # Count the white, black, and non-binary pixels.
+    # ImageJ ROIs are not one color if made on a diagonal.
+    for i in range(im.width):
+        for j in range(im.height):
+            px = im.getpixel((i, j))
+            if px == (255, 255, 255):
+                count_1 += 1
+            elif px == (0, 0, 0):
+                count_0 += 1
+            else:
+                count_red += 1
+    try:
+        normalized_area = (500 * count_1) / tile_area_list[tile_position]
+    except ZeroDivisionError:
+        normalized_area = (500 * count_1) / (tile_dims[0] * tile_dims[1])
+
+    # extracts the color corresponding to each value 1-500 in the colormap.
+    cmap_color_hex = matplotlib.colors.rgb2hex(my_colormap(round(normalized_area)))
+    cmap_color_rgb = tuple(int(cmap_color_hex.lstrip('#')[i:i + 2], 16) for i in (0, 2, 4))
+
+    for k in range(im.width):
+        for l in range(im.height):
+            if pixel_map[k, l] == (255, 255, 255):
+                pixels_new[k, l] = cmap_color_rgb
+            elif pixel_map[k, l] != (0, 0, 0):
+                pixels_new[k, l] = (0, 0, 0)
+            else:
+                pixels_new[k, l] = pixel_map[k, l]
+    img.save(f'recolored_tiles/{image_path.split("/")[1]}')
+
+
+def re_stitch():
+    stitched_image = Image.new('RGB', (large_image.width, large_image.height))
+    image_list = os.listdir('recolored_tiles')
+    current_tile = 0
+
+    for y in range(0, large_image.height - 1, tile_dims[0]):
+        for x in range(0, large_image.width - 1, tile_dims[1]):
+            tile_image = Image.open(f'recolored_tiles/{image_list[current_tile]}')
+            stitched_image.paste(tile_image, (x, y), 0)
+            current_tile += 1
+
+    stitched_image.save(f'stitched_{IMAGE_PATH}.png')
+    print('Done Stitching Colored Tiles')
+
+
+def make_histogram():
+    fig = plt.figure()
+    plt.hist(tile_area_list, bins=50)
+    plt.title('Frequency Distribution of Astrocyte Densities in Local Retina Regions\nRetina:{FileName}')
+    plt.xlabel('GFAP Tile Density')
+    plt.ylabel('Frequency')
+    plt.savefig('histogram.jpg')
+    print('Done Making Histogram')
+
+
+def mask_roi_area():
+    mask = Image.open('mask.png')
+    if mask.mode != 'L':
+        mask.convert('L')
+    split_grid(directory='mask_tiles', full_image_path='mask.png')
+
+    global tile_area_list
+    tile_area_list = []
+
+    for t in os.listdir('mask_tiles'):
+        area_tile = Image.open(f'mask_tiles/{t}')
+        area_tile_dims = (area_tile.height, area_tile.width)
+        if set(area_tile.getdata()) == {0}:
+            tile_area_list.append(0)
+        else:
+            white_area = 0
+            for q in range(area_tile_dims[1]):
+                for r in range(area_tile_dims[0]):
+                    px = area_tile.getpixel((q, r))
+                    if px != 0:
+                        white_area += 1
+            tile_area_list.append(white_area)
+    print('Done Getting Mask Area')
+
+
+def make_background_transparent():
+    """Makes any pixel that is black transparent"""
+    im = Image.open('stitched_binary thresholded.tif.png')
+    im = im.convert('RGBA')
+    pixel_map_t = im.load()
+    img_t = Image.new(im.mode, im.size)
+    pixels_new_t = img_t.load()
+
+    for m in range(im.width):
+        for n in range(im.height):
+            if pixel_map_t[m, n] == (0, 0, 0, 255):
+                pixels_new_t[m, n] = (0, 0, 0, 0)
+            else:
+                pixels_new_t[m, n] = pixel_map_t[m, n]
+    img_t.save('ttt.png')
+    print('Done Making Background Transparent')
+
+
+# ////////////////////////////////////////////////Program///////////////////////////////////////////////////////////////
+start_time = time.time()
+
+split_grid(directory='tiles', full_image_path=IMAGE_PATH)
+mask_roi_area()
+
+step = 0
+for tile in os.listdir('tiles'):
+    get_binary_density_and_recolor(image_path=f'tiles/{tile}', tile_position=step)
+    step += 1
+    print(step)
+
+print('Finished Calculating Densities and Coloring Tiles')
+
+re_stitch()
+make_histogram()
+
+# make_background_transparent()
+
+print("--- %s seconds ---" % round((time.time() - start_time), 3))
+
+# //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
